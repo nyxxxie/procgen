ae25783a5713ff1198753f10fd536e9b7f947b15
nyxxie <nyxxxxie@gmail.com>
2018-12-02 11:09:32 -0500

    Got indices working.

--- renderer/src/engine/mesh.rs
+++ renderer/src/engine/mesh.rs
@@ -5,16 +5,18 @@
 // Also, create a premade mesh generator (colored cube, textured cube, cylinder, quad, etc)
 extern crate gl;
 
+use std;
 use std::mem::size_of;
 use engine::buffer::Buffer;
 use engine::shader::Program;
 use engine::vao::VertexArray;
-use gl::types::{ GLuint, GLint, GLvoid };
+use gl::types::{ GLuint, GLint, GLfloat, GLvoid };
 
 /// Represents a mesh of vertices.
 pub struct Mesh {
     vao: VertexArray,
-	index_amt: usize,
+    uses_indices: bool,
+	element_amt: usize,
 	shader: Program,
 }
 
@@ -22,12 +24,22 @@ impl Mesh {
 	pub fn draw(&self, mode: GLuint) {
 		self.vao.bind();
 		self.shader.activate();
-		unsafe {
-            gl::DrawArrays(
-                mode,
-                0,  // starting index
-                self.index_amt as GLint);
-		}
+        if self.uses_indices {
+		    unsafe {
+                gl::DrawElements(
+                    mode,
+                    self.element_amt as GLint,
+                    gl::UNSIGNED_INT,
+                    std::ptr::null());
+		    }
+        } else {
+		    unsafe {
+                gl::DrawArrays(
+                    mode,
+                    0,  // starting index
+                    self.element_amt as GLint);
+		    }
+        }
 		self.vao.unbind();
 	}
 
@@ -47,8 +59,8 @@ struct Attribute {
 
 /// Stuff needed to build a mesh.
 pub struct MeshBuilder {
-	vertices: Vec<f32>,
-	indices: Vec<u32>,
+	vertices: Vec<GLfloat>,
+	indices: Vec<GLuint>,
 	attributes: Vec<Attribute>,
 	shader: Option<Program>,
 }
@@ -63,30 +75,38 @@ impl MeshBuilder {
 		};
 	}
 
+				///* First triangle */
+				//-0.5,  0.5, 0.0,  // upper left
+				// 0.5,  0.5, 0.0,  // upper right
+				//-0.5, -0.5, 0.0,  // lower left
+				///* Second triangle */
+				//-0.5, -0.5, 0.0,  // lower left
+				// 0.5, -0.5, 0.0,  // lower right
+				// 0.5,  0.5, 0.0,  // upper right
 	pub fn new_quad() -> MeshBuilder {
 		// TODO: use indices
 		return MeshBuilder::new()
 			.vertex_data(&[
-				/* First triangle */
 				-0.5,  0.5, 0.0,  // upper left
 				 0.5,  0.5, 0.0,  // upper right
 				-0.5, -0.5, 0.0,  // lower left
-				/* Second triangle */
-				-0.5, -0.5, 0.0,  // lower left
 				 0.5, -0.5, 0.0,  // lower right
-				 0.5,  0.5, 0.0,  // upper right
 			])
+            .indices(&[
+                0, 1, 2,
+                1, 2, 3,
+            ])
 			.attribute(0, 3);
     }
 
-	pub fn vertex_data(mut self, vertices: &[f32]) -> MeshBuilder {
+	pub fn vertex_data(mut self, vertices: &[GLfloat]) -> MeshBuilder {
 		for vertex in vertices {
 			self.vertices.push(*vertex);
 		}
 		return self;
 	}
 
-	pub fn indices(mut self, indices: &[u32]) -> MeshBuilder {
+	pub fn indices(mut self, indices: &[GLuint]) -> MeshBuilder {
 		for index in indices {
 			self.indices.push(*index);
 		}
@@ -113,26 +133,18 @@ impl MeshBuilder {
 		let vao: VertexArray = VertexArray::new();
 		vao.bind();
 
-		/* Create vertex buffer and insert our data */
-		let vbo: Buffer = Buffer::new_array_buffer();
-		vbo.bind();
-		vbo.set_data_static_draw(&self.vertices);
-
 		/* Calculate row size */
 		let mut row_size: usize = 0;
 		for attribute in &self.attributes {
-			row_size += (attribute.component_amt as usize) * size_of::<f32>();
+			row_size += (attribute.component_amt as usize) * size_of::<GLfloat>();
         }
 
-		/* Determine the amount of indices to render */
-		let mut index_amt = self.indices.len();
-		if index_amt == 0 {
-			index_amt = (self.vertices.len() * size_of::<f32>()) / row_size;
-		}
+		/* Create vertex buffer and insert our data */
+		let vbo_verts: Buffer = Buffer::new_array_buffer();
+		vbo_verts.bind();
+		vbo_verts.set_data_static_draw(&self.vertices);
 
-		// TODO: handle indices
-
-		/* Set attributes */
+		/* Specify the format of provided vertex data */
 		let mut col_offset: usize = 0;
 		for attribute in &self.attributes {
 		    unsafe {
@@ -147,12 +159,35 @@ impl MeshBuilder {
 		    }
 
 			/* Increase column offset by the amount of space the previous column occupied */
-			col_offset += (attribute.component_amt as usize) * size_of::<f32>();
+			col_offset += (attribute.component_amt as usize) * size_of::<GLfloat>();
 		}
 
-		/* Unbind our opengl objects for safety */
-		vbo.unbind();
-		vao.unbind();
+
+        /* Create an index buffer if we were provided indices */
+        let mut uses_indices: bool = false;
+		let mut element_amt = 0;
+        if self.indices.len() > 0 {
+            /* Create index buffer */
+		    let vbo_indices: Buffer = Buffer::new_element_array_buffer();
+		    vbo_indices.bind();
+		    vbo_indices.set_data_static_draw(&self.indices);
+            uses_indices = true;
+
+            /* Set element amount to be equal to the number of provided indices */
+		    element_amt = self.indices.len();
+
+            /* Unbind opengl objects */
+		    vbo_verts.unbind();
+		    vao.unbind();
+		    vbo_indices.unbind();  // NOTE: this must be unbound AFTER the vao, otherwise we'll lose the vao reference
+        } else {
+            /* If there are no indices, calculate element amount from the number of vertices */
+			element_amt = (self.vertices.len() * size_of::<GLfloat>()) / row_size;
+
+            /* Unbind opengl objects */
+		    vbo_verts.unbind();
+		    vao.unbind();
+        }
 
 		/* Create and return our new mesh */
 		// TODO: is it safe to let the vbo go out of scope and be deleted?
@@ -160,7 +195,8 @@ impl MeshBuilder {
 		// references it
 		return Mesh{
 			vao,
-			index_amt,
+            uses_indices,
+			element_amt,
 			shader: self.shader.unwrap()
 		};
 	}
